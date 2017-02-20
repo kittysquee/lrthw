b0d6feb53b3369d3d49406304ac83e8450e30be8
Elaine Osbourn <kittysquee@gmail.com>
2017-02-20 13:40:37 +0000

    Adds the contents of exercise 11

---
+++ chap11/ex11.rb
@@ -0,0 +1,19 @@
+# print "How old are you?"
+# age = gets.chomp
+# print "How tall are you?"
+# height = gets.chomp
+# print "How much do you weigh?"
+# weight = gets.chomp
+#
+# puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What is your name?"
+name = gets.chomp
+print "Who is your favourite cat?"
+first_cat = gets.chomp
+print "Who is your second favourite cat?"
+second_cat = gets.chomp
+print "Who is your third favourite cat?"
+third_cat = gets.chomp
+
+puts "So you're saying #{first_cat} is your most favourite, #{second_cat} is your second favourite and that you hate #{third_cat}! Playing favourites isn't very nice #{name}!"
