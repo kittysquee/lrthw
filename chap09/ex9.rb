854bfc14211846eb44db282cbf9837f3181aeb6e
Elaine Osbourn <kittysquee@gmail.com>
2017-02-20 13:36:46 +0000

    Adds contents of exercise 9

---
+++ chap09/ex9.rb
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# the \n means new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# I am assuming that this is some type of formatting tool
+puts %q{
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5 or 6.
+}
