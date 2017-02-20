ab30783eb4395e6df2a874bf96a9645750bad7ad
Elaine Osbourn <kittysquee@gmail.com>
2017-02-20 13:44:38 +0000

    Adds contents of exercise 13

---
+++ chap13/ex13.rb
@@ -0,0 +1,10 @@
+print "Could you tell me your name?"
+name = $stdin.gets.chomp
+
+puts "Your name is #{name}."
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
