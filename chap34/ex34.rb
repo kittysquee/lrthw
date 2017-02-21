f688bd165c42b4f7fc66e6c713a4b200a04c0bc9
Elaine Osbourn <kittysquee@gmail.com>
2017-02-21 16:00:35 +0000

    Adds content of exercise 34

---
+++ chap34/ex34.rb
@@ -0,0 +1,18 @@
+animals = ["bear", "ruby", "peacock", "kangaroo", "whale", "platypus"]
+
+# 1. The animal at one is ruby.
+puts animals[1]
+# 2. The third (3rd) animal is at 2 and is a peacock (crosses fingers!)
+puts animals[2]
+# 3. The first (1st) animal is at 0 and is a bear.
+puts animals[0]
+# 4. The animal at 3 is a kangaroo.
+puts animals[3]
+# 5. The fifth (5th) animal is at 4 and is a whale.
+puts animals[4]
+# 6. The animal at 2 is a peacock.
+puts animals[2]
+# 7. The sixth animal is at 5 and is a platypus
+puts animals[5]
+# 8. The animal at 4 is a whale.
+puts animals[4]
---
+++ chap34/ex34_extra.rb
@@ -0,0 +1,18 @@
+cats = ["Pixel", "BMO", "Tia", "Smudge", "Honey", "Mischief", "Sooty", "Grumpy Cat", "Nala", "Lil Bub"]
+
+# Who is the 2nd cat? bmo
+puts cats[1]
+# Who is cat 4? honey
+puts cats[4]
+# Who is the fifth cat? honey
+puts cats[4]
+# Who is cat 7? grumpy cat
+puts cats[7]
+# Who is cat 0? The Queen herself. The original Pixel.
+puts cats[0]
+# Who is the second cat? Bumble
+puts cats[1]
+# Who is cat 5? Mischief
+puts cats[5]
+# Who is the 4th cat? Smudge
+puts cats[3]
